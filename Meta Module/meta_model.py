2687a648fe9ab26df533e7b9153be9589718f194
Hrishabh-Punetha <60377183+Hrishabh-Punetha@users.noreply.github.com>
2021-04-01 12:51:43 +0530

    First Commit

--- Meta Module/meta_model.py
+++ Meta Module/meta_model.py
@@ -1,1 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+data=pd.read_csv("Surgical-deepnet.csv")
+
+
+# In[3]:
+
+
+data.isnull().sum()
+
+
+# In[4]:
+
+
+data.dtypes
+
+
+# In[5]:
+
+
+data['gender'].value_counts()
+
+
+# In[6]:
+
+
+data.head()
+
+
+# In[7]:
+
+
+data.describe()
+
+
+# In[12]:
+
+
+plt.plot(data.mean())
+
+
+# In[11]:
+
+
+plt.plot(data.var())
+
+
+# In[8]:
+
+
+plt.plot(data.skew())
+
+
+# In[9]:
+
+
+plt.plot(data.kurtosis())
+
+
+# In[ ]:
+
+
+
 
